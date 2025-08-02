9eb9bf525e9507c9d565515c5aa86192e93fa6ec
Ryan Yeske <ryan@ryanyeske.com>
2025-08-01 18:48:58 -0700

    Show details of game on chess page

--- handlers/fun/chess/match.go
+++ handlers/fun/chess/match.go
@@ -1,11 +1,13 @@
 package chess
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"oj/api"
 	"oj/handlers/layout"
 	"oj/internal/link"
+	"oj/internal/middleware/auth"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
@@ -14,8 +16,8 @@ import (
 )
 
 func (s *service) HandleMatch(w http.ResponseWriter, r *http.Request) {
-	l := layout.FromContext(r.Context())
 	ctx := r.Context()
+	l := layout.FromContext(ctx)
 	matchID, _ := strconv.Atoi(chi.URLParam(r, "matchID"))
 	match, err := s.Queries.ChessMatchByID(ctx, int64(matchID))
 	if err != nil {
@@ -29,20 +31,39 @@ func (s *service) HandleMatch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	currentUser := auth.FromContext(ctx)
+	opponent, err := matchOpponent(ctx, s.Queries, match, currentUser.ID)
+
 	uiGameState := UIGameState{gameState: board, flip: l.User.ID == match.BlackUserID}
-	layout.Layout(l, "chess club", chessPageNode(match, uiGameState)).Render(w)
+	elMatchID := fmt.Sprintf("match-%d", match.ID)
+
+	layout.Layout(l, "chess club",
+		h.Div(h.ID(elMatchID),
+			g.Attr("hx-get", link.ChessMatch(match.ID)),
+			g.Attr("hx-trigger", "USER_UPDATE from:body"),
+			g.Attr("hx-target", "#"+elMatchID),
+			g.Attr("hx-select", "#"+elMatchID),
+			g.Attr("hx-swap", "outerHTML"),
+			h.H4(h.Style("padding-left: 1em; background:white; foreground:black"),
+				g.Text(fmt.Sprintf("%s v %s", opponent.Username, currentUser.Username))),
+			h.Div(h.Style("display:flex; gap:1em"),
+				h.Div(h.ID("board-container"), h.Style("height: 80vh; width: 80vh;"),
+					uiGameState,
+				),
+				h.Div(h.Style("flex:1"), g.Text(uiGameState.gameState.Game.String())),
+			),
+		),
+	).Render(w)
 }
 
-func chessPageNode(match api.ChessMatch, uiGameState UIGameState) g.Node {
-	elMatchID := fmt.Sprintf("match-%d", match.ID)
-	return h.Div(h.ID(elMatchID),
-		g.Attr("hx-get", link.ChessMatch(match.ID)),
-		g.Attr("hx-trigger", "USER_UPDATE from:body"),
-		g.Attr("hx-target", "#"+elMatchID),
-		g.Attr("hx-select", "#"+elMatchID),
-		g.Attr("hx-swap", "outerHTML"),
-		h.H1(g.Text("chess club")),
-		h.Div(h.ID("board-container"), h.Style("height: 80vh; width: 80vh;"),
-			uiGameState,
-		))
+func matchOpponent(ctx context.Context, qtx *api.Queries, match api.ChessMatch, userID int64) (*api.User, error) {
+	opponentID := match.WhiteUserID
+	if userID == match.WhiteUserID {
+		opponentID = match.BlackUserID
+	}
+	opponent, err := qtx.UserByID(ctx, opponentID)
+	if err != nil {
+		return nil, fmt.Errorf("UserByID", err)
+	}
+	return &opponent, nil
 }
